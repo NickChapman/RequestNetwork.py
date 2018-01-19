ed9ec002384dab422bbbd1a0ece13ac414c8e186
domen-doma <domen.koneski@gmail.com>
2018-01-19 22:49:49 +0100

    Exception messages added to ipfs_service.py

--- servicesExternal/ipfs_service.py
+++ servicesExternal/ipfs_service.py
@@ -23,8 +23,8 @@ class Ipfs:
         self.ipfs = ipfsapi.connect(self.ipfsConfig["host"], self.ipfsConfig["port"], self.ipfsConfig["protocol"])
 
     async def add_file(self, data: str):
-        if data == None or data == "":
-            raise ValueError("")
+        if data is None or data == "":
+            raise ValueError("Data is None or invalid.")
         
         data_parsed = json.loads(data)
         result = await self.ipfs.add(json.dumps(data_parsed, separators=(',',':')))
@@ -32,12 +32,24 @@ class Ipfs:
         return result["hash"]
 
     async def get_file(self, hash: str):
-        if hash == None or hash == "":
-            raise ValueError("")
+        if hash is None or hash == "":
+            raise ValueError("Hash is None or invalid.")
         
         try:
             result = await self.ipfs.cat(hash)
 
             return result
+        except ipfsapi.exceptions.ConnectionError as err:
+            print("IPFS connection error: ", err)
+        except ipfsapi.exceptions.ProtocolError as err:
+            print("Bad connection protocol: ", err)
+        except ipfsapi.exceptions.ErrorResponse as err:
+            print("Error retrieving file: ", err)
+        except ipfsapi.exceptions.StatusError as err:
+            print("Status error: ", err)
+        except ipfsapi.exceptions.TimeoutError as err:
+            print("Connection timeout error: ", err)
         except ipfsapi.exceptions.Error as err:
-            raise err
+            print("Error retrieving file: ", err)
+        finally:
+            return None
