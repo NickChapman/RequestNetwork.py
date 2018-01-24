170c6fc90fdcc760c2f565c36d45a319946dc499
Bander Alsulami <bander@alsulamy.com>
2018-01-23 21:36:47 -0500

    servicesContracts intial implementation. It needs another round when artifacts module is implemented.

--- servicesContracts.py
+++ servicesContracts.py
@@ -1,10 +1,28 @@
 from typing import Any
-
-from artifacts import *
 from servicesContracts.requestEthereum_service import RequestEthereumService
 
+"""
+  getServiceFromAddress return the service of a coresponding currency contract address
+  :param  address:
+        The address of the currency contract
+"""
 def getServiceFromAddress(address: str) -> Any:
-    pass
+    if not address:
+        return None
+    if isThisArtifact(Artifacts.requestEthereumArtifact, address):
+        return RequestEthereumService()
 
+"""
+  isThisArtifact return
+  :param  artifact:
+      RequestNetwork Artifact to use in its interactions with the Ethereum network
+    :param  address:
+        The address of the currency contract
+"""
 def isThisArtifact(artifact, address: str) -> bool:
-    pass
+    if not address:
+        return False
+    sanitizedAdress = address.lower()
+    # assume artifact.networks is a dictionary
+    return any((value.address.lower() == sanitizedAdress if not value.address else False
+                for key,value in artifact.networks.items()))
