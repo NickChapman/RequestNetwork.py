623dcdeb371fcbdef7d756d26af316cc7ddcf33d
Justice Suh <justice.suh@gmail.com>
2018-01-24 13:46:27 -0500

    getRequest

--- servicesCore/requestCore_service.py
+++ servicesCore/requestCore_service.py
@@ -1,10 +1,14 @@
+import asyncio
+
 from artifacts import *
 from config import config
 from servicesContracts.requestEthereum_service import RequestEthereumService
+from servicesExtensions import getServiceFromAddress
 from servicesExtensions.requestSyncrhoneExtensionEscrow_service import RequestSynchroneExtensionEscrowService
 from servicesExternal.ipfs_service import Ipfs
 from servicesExternal.web3_single import Web3Single
 
+EMPTY_BYTES_32 = '0x0000000000000000000000000000000000000000'
 
 class RequestCoreService:
     def __init__(self):
@@ -16,17 +20,61 @@ class RequestCoreService:
         self._addressRequestCore = requestCoreArtifact.networks[self._web3Single.networkName].address
         self._instanceRequestCore = self._web3Single.web3.eth.Contract(self._abiRequestCore, self._addressRequestCore)
 
-    def getCurrentNumRequest(self):
-        pass
+    async def getCurrentNumRequest(self):
+        try:
+            return self._instanceRequestCore.call().numRequests()
+        except Exception as e:
+            raise e
 
-    def getVersion(self):
-        pass
+    async def getVersion(self):
+        try:
+            return self._instanceRequestCore.call().VERSION()
+        except Exception as e:
+            raise e
 
     def getCollectEstimation(self, expectedAmount: any, currencyContract: str, extension: str):
         pass
 
-    def getRequest(self, requestId: str):
-        pass
+    async def getRequest(self, requestId: str):
+        if not self._web3Single.isHexStrictBytes32(requestId):
+            raise ValueError('requestId must be a 32 bytes hex string')
+        try:
+            data = self._instanceRequestCore.call().requests(requestId)
+            if data.creator == EMPTY_BYTES_32:
+                raise ValueError('request not found')
+
+            # excluding BN
+            dataResult = {
+                'creator': data.creator,
+                'currencyContract': data.currencyContract,
+                'data': data.data,
+                'extension': data.extension != EMPTY_BYTES_32 ? data.extension : None,
+                'payee': data.payee,
+                'payer': data.payer,
+                'requestId': requestId,
+                'state': int(data.state)
+            }
+
+            # get information from the currency contract
+            if getServiceFromAddress(data.currencyContract):
+                ccyContractDetails = await getServiceFromAddress(data.currencyContract).getRequestCurrencyContractInfo(requestId)
+                dataResult['currencyContract'] = ccyContractDetails
+
+            # get information from the extension contract
+            if data.extension and data.extension != '' and getServiceFromAddress(data.extension):
+                extensionDetails = await getServiceFromAddress(data.extension).getRequestExtensionInfo(requestId)
+                dataResult['extension'] = extensionDetails
+
+            # get ipfs details if needed
+            if dataResult.data && dataResult.data != '':
+                # might need to do some json wrangling
+                dataResult['data'] = await self._ipfs.get_file(dataResult.data)
+            else:
+                dataResult['data'] = None
+
+            return dataResult
+        except Exception as e:
+            raise e
 
     def getRequestByTransactionHash(self, hash:str):
         pass
